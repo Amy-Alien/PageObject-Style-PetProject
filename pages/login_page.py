30c710969dbbcf8eb2c2a9e63beaa817a4426f56
amyalien <hughsence@gmail.com>
2026-03-01 23:08:17 +0400

    Added login page tests

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from conftest import browser
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        URL = self.browser.current_url
+        assert ("selenium1py.pythonanywhere.com" in URL
+                and "/accounts/login/" in URL), "Not open login page"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form not present"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form not present"
