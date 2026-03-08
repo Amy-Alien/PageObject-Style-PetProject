18814efb368d03d84cf29064a60e98f4be4ca72b
amyalien <hughsence@gmail.com>
2026-03-08 14:14:37 +0400

    Added basket page tests and test grouping and setup

--- test_main_page.py
+++ test_main_page.py
@@ -8,8 +8,8 @@ from pages.basket_page import BasketPage
 link = "http://selenium1py.pythonanywhere.com/"
 
 @pytest.mark.login_guest
-class TestLoginFromMainPage(BasePage):
-    def test_guest_can_go_to_login_page(browser):
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
@@ -17,7 +17,7 @@ class TestLoginFromMainPage(BasePage):
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
-    def test_guest_should_see_login_link(browser):
+    def test_guest_should_see_login_link(self, browser):
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
--- test_product_page.py
+++ test_product_page.py
@@ -22,6 +22,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, link)
         page.open()
@@ -39,6 +40,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -56,6 +58,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -64,6 +67,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
